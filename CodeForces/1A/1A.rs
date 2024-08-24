7f98bc9b735ab7e4bac5d78f773a843f2117dd0e
Evan Chen <evan@evanchen.cc>
2024-08-23 17:14:02 -0700

    Add 1A for kicks

---
+++ CodeForces/1A/1A.rs
@@ -0,0 +1,45 @@
+/* {{{ https://codeforces.com/blog/entry/67391
+ * vim:fdm=marker
+ */
+#[allow(unused_imports)]
+use std::cmp::{max, min};
+use std::io::{stdin, stdout, BufWriter, Write};
+
+#[derive(Default)]
+struct Scanner {
+    buffer: Vec<String>,
+}
+impl Scanner {
+    fn next<T: std::str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buffer.pop() {
+                return token.parse().ok().expect("oh no failed to parse");
+            }
+            let mut input = String::new();
+            stdin().read_line(&mut input).expect("eep i failed to read");
+            self.buffer = input.split_whitespace().rev().map(String::from).collect();
+        }
+    }
+}
+
+#[allow(dead_code)]
+fn debug<T>(value: T)
+where
+    T: std::fmt::Debug, // T must implement the Debug trait
+{
+    #[cfg(feature = "debug")]
+    dbg!(value);
+}
+/* }}} */
+
+#[allow(dead_code)]
+fn main() {
+    let mut scan = Scanner::default();
+    let out = &mut BufWriter::new(stdout());
+
+    // write your program here, e.g.
+    let n: u64 = scan.next();
+    let m: u64 = scan.next();
+    let a: u64 = scan.next();
+    writeln!(out, "{}", n.div_ceil(a) * m.div_ceil(a)).ok();
+}
